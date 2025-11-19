beb37c12e52017b851415978080b779b5733e25d
Anthony Ford <you@example.com>
2025-11-18 22:17:44 -0500

    Save

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemList = document.getElementById('item-list');
 const itemInput = document.getElementById('item-input');
+const itemClear = document.getElementById('btn-clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e) {
     e.preventDefault();
@@ -36,4 +39,30 @@ function createIcon(classes) {
     return icon;
 }
 
+
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm('Are you sure?')) {
+       e.target.parentElement.parentElement.remove();
+    }
+
+    function itemClear() {
+        while (itemList.firstChild) {
+            itemList.removeChild(itemList.firstChild);
+        }
+
+function checkUI () {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        itemClear.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        itemClear.style.display = 'block';
+        itemFilter.style.display = 'block';
+    } 
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+itemClear.addEventListener('click', clearItems); 
+
+checkUI();
